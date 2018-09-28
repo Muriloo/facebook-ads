3ae9b21a61355fdfa7720300990bd0a8fb0f2d24
Murilo Nigris <murilo.nigris@gmail.com>
2018-09-27 22:31:34 -0300

    removing duplicates from staging_fb_ads

--- models/transformation/staging_fb_ads.sql
+++ models/transformation/staging_fb_ads.sql
@@ -10,17 +10,20 @@ with ads_xf_ads as (
     row_number() over (partition by id order by updated_at) as version_number
   from ads_xf_ads
 
+), final_t as (
+  select
+    *,
+    case
+      when version_number = 1 then created_at
+      when version_number > 1 then updated_at
+    end as effective_from,
+    case
+      when version_number = num_versions then null
+      else lead(updated_at) over (partition by id order by updated_at)
+    end as effective_to,
+    md5(id || '|' || version_number) as unique_id
+  from ads_xf_windowed
 )
-
-select
-  *,
-  case
-    when version_number = 1 then created_at
-    when version_number > 1 then updated_at
-  end as effective_from,
-  case
-    when version_number = num_versions then null
-    else lead(updated_at) over (partition by id order by updated_at)
-  end as effective_to,
-  md5(id || '|' || version_number) as unique_id
-from ads_xf_windowed
+select *
+from final_t
+where num_versions = version_number
