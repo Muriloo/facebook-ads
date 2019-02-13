8c2f3b0bba44e19ba0f51541bacf908de700a253
Murilo Nigris <murilo.nigris@gmail.com>
2019-02-13 00:13:51 -0200

    adding etl for spending by region

---
+++ macros/stitch/base/stitch_fb_ad_insights_region.sql
@@ -0,0 +1,223 @@
+{% macro stitch_fb_ad_insights_region() %}
+
+    {{ adapter_macro('facebook_ads.stitch_fb_ad_insights_region') }}
+
+{% endmacro %}
+
+
+{% macro default__stitch_fb_ad_insights_region() %}
+
+select
+  date_start::date as date_day,
+  nullif(account_id,'') as account_id,
+  nullif(account_name,'') as account_name,
+  nullif(ad_id,'') as ad_id,
+  nullif(adset_id,'') as adset_id,
+  nullif(campaign_id,'') as campaign_id,
+  nullif(region,'') as region,
+  impressions,
+  clicks,
+  unique_clicks,
+  spend,
+  -- frequency,
+  reach,
+  '{{ var("account_1_currency")}}' as account_currency,
+  {{ var("account_1_store_id")}} as dim_store_fk
+  -- nullif(objective,'') as objective,
+  -- canvas_avg_view_percent,
+  -- canvas_avg_view_time,
+  -- inline_link_clicks,
+  -- inline_post_engagement,
+  -- unique_inline_link_clicks
+from
+  {{ var('account_1_schema') }}.{{ var('ads_insights_region_table') }}
+
+UNION ALL
+
+select
+  date_start::date as date_day,
+  nullif(account_id,'') as account_id,
+  nullif(account_name,'') as account_name,
+  nullif(ad_id,'') as ad_id,
+  nullif(adset_id,'') as adset_id,
+  nullif(campaign_id,'') as campaign_id,
+  nullif(region,'') as region,
+  impressions,
+  clicks,
+  unique_clicks,
+  spend,
+  -- frequency,
+  reach,
+  '{{ var("account_2_currency")}}' as account_currency,
+  {{ var("account_2_store_id")}} as dim_store_fk
+  -- nullif(objective,'') as objective,
+  -- canvas_avg_view_percent,
+  -- canvas_avg_view_time,
+  -- inline_link_clicks,
+  -- inline_post_engagement,
+  -- unique_inline_link_clicks
+from
+  {{ var('account_2_schema') }}.{{ var('ads_insights_region_table') }}
+
+UNION ALL
+
+select
+  date_start::date as date_day,
+  nullif(account_id,'') as account_id,
+  nullif(account_name,'') as account_name,
+  nullif(ad_id,'') as ad_id,
+  nullif(adset_id,'') as adset_id,
+  nullif(campaign_id,'') as campaign_id,
+  nullif(region,'') as region,
+  impressions,
+  clicks,
+  unique_clicks,
+  spend,
+  -- frequency,
+  reach,
+  '{{ var("account_3_currency")}}' as account_currency,
+  {{ var("account_3_store_id")}} as dim_store_fk
+  -- nullif(objective,'') as objective,
+  -- canvas_avg_view_percent,
+  -- canvas_avg_view_time,
+  -- inline_link_clicks,
+  -- inline_post_engagement,
+  -- unique_inline_link_clicks
+from
+  {{ var('account_3_schema') }}.{{ var('ads_insights_region_table') }}
+
+{% endmacro %}
+
+
+{% macro snowflake__stitch_fb_ad_insights_region() %}
+
+select
+  date_start::date as date_day,
+  account_id,
+  account_name as account_name,
+  ad_id,
+  adset_id,
+  campaign_id,
+  region,
+  impressions,
+  clicks,
+  unique_clicks,
+  spend,
+  -- frequency,
+  reach,
+  '{{ var("account_1_currency")}}' as account_currency,
+  {{ var("account_1_store_id")}} as dim_store_fk
+  -- nullif(objective,'') as objective,
+  -- canvas_avg_view_percent,
+  -- canvas_avg_view_time,
+  -- inline_link_clicks,
+  -- inline_post_engagement,
+  -- unique_inline_link_clicks
+from {{ var('account_1_schema') }}.{{ var('ads_insights_region_table') }}
+
+UNION ALL
+
+select
+  date_start::date as date_day,
+  account_id,
+  account_name as account_name,
+  ad_id,
+  adset_id,
+  campaign_id,
+  region,
+  impressions,
+  clicks,
+  unique_clicks,
+  spend,
+  -- frequency,
+  reach,
+  '{{ var("account_2_currency")}}' as account_currency,
+  {{ var("account_2_store_id")}} as dim_store_fk
+  -- nullif(objective,'') as objective,
+  -- canvas_avg_view_percent,
+  -- canvas_avg_view_time,
+  -- inline_link_clicks,
+  -- inline_post_engagement,
+  -- unique_inline_link_clicks
+from {{ var('account_2_schema') }}.{{ var('ads_insights_region_table') }}
+
+UNION ALL
+
+select
+  date_start::date as date_day,
+  account_id,
+  account_name as account_name,
+  ad_id,
+  adset_id,
+  campaign_id,
+  region,
+  impressions,
+  clicks,
+  unique_clicks,
+  spend,
+  -- frequency,
+  reach,
+  '{{ var("account_3_currency")}}' as account_currency,
+  {{ var("account_3_store_id")}} as dim_store_fk
+  -- nullif(objective,'') as objective,
+  -- canvas_avg_view_percent,
+  -- canvas_avg_view_time,
+  -- inline_link_clicks,
+  -- inline_post_engagement,
+  -- unique_inline_link_clicks
+from {{ var('account_3_schema') }}.{{ var('ads_insights_region_table') }}
+
+
+UNION ALL
+
+select
+  date_start::date as date_day,
+  account_id,
+  account_name as account_name,
+  ad_id,
+  adset_id,
+  campaign_id,
+  region,
+  impressions,
+  clicks,
+  unique_clicks,
+  spend,
+  -- frequency,
+  reach,
+  '{{ var("account_4_currency")}}' as account_currency,
+  {{ var("account_4_store_id")}} as dim_store_fk
+  -- nullif(objective,'') as objective,
+  -- canvas_avg_view_percent,
+  -- canvas_avg_view_time,
+  -- inline_link_clicks,
+  -- inline_post_engagement,
+  -- unique_inline_link_clicks
+from {{ var('account_4_schema') }}.{{ var('ads_insights_region_table') }}
+
+
+UNION ALL
+
+select
+  date_start::date as date_day,
+  account_id,
+  account_name as account_name,
+  ad_id,
+  adset_id,
+  campaign_id,
+  region,
+  impressions,
+  clicks,
+  unique_clicks,
+  spend,
+  -- frequency,
+  reach,
+  '{{ var("account_5_currency")}}' as account_currency,
+  {{ var("account_5_store_id")}} as dim_store_fk
+  -- nullif(objective,'') as objective,
+  -- canvas_avg_view_percent,
+  -- canvas_avg_view_time,
+  -- inline_link_clicks,
+  -- inline_post_engagement,
+  -- unique_inline_link_clicks
+from {{ var('account_5_schema') }}.{{ var('ads_insights_region_table') }}
+{% endmacro %}
---
+++ models/marketing/ft_fb_ad_performance_platform_region.sql
@@ -0,0 +1,46 @@
+with ads as (
+
+  select * from {{ref('staging_fb_ads')}}
+
+), insights as (
+
+  select * from {{ref('b_fb_ad_insights_region')}}
+
+), campaigns as (
+
+  select * from {{ref('b_fb_campaigns')}}
+
+), adsets as (
+
+  select * from {{ref('staging_fb_adsets')}}
+
+), final as (
+
+  select
+    c.name as campaign_name
+    ,s.name as adset_name
+    ,a.name as ad_name
+    ,ai.date_day as campaign_date
+    ,ai.account_name as account_name
+    ,ai.account_currency as account_currency
+    ,ai.dim_store_fk as dim_store_fk
+    ,ai.region as region_name
+    ,'facebook' as data_source
+    ,'Paid' as traffic_type
+    ,'Paid Social' as group_channel
+    ,'paid_social' as medium
+    ,'facebook' as "source"
+    ,sum(impressions) as impressions
+    ,sum(clicks) as clicks
+    ,sum(unique_clicks) as unique_clicks
+    ,sum(spend) as spend -- spend in account_currency
+  from insights ai
+  left join campaigns c on c.id = ai.campaign_id
+  left join adsets s on s.id = adset_id
+  left join ads a on a.id = ai.ad_id
+  group by 1,2,3,4,5,6,7,8
+)
+
+select
+  *
+from final
